945106624cec40282705e0c5a82c84b095660a23
vvalyavskiy <slava.val.al@gmail.com>
2014-09-17 18:26:44 +0400

    wrong name of object priperty

--- nailgun/nailgun/orchestrator/deployment_serializers.py
+++ nailgun/nailgun/orchestrator/deployment_serializers.py
@@ -824,7 +824,7 @@ class NeutronNetworkDeploymentSerializer(NetworkDeploymentSerializer):
         for iface in node.interfaces:
             for ng in iface.assigned_networks_list:
                 if ng.name in storage_ng_names:
-                    attrs['storage_network_data'][ng.name + '_vlan'] = iface.vlan
+                    attrs['storage_network_data'][ng.name + '_vlan'] = iface.vlan_start
                     if ng.name in iscsi_ng_names:
                         attrs['storage_network_data'][ng.namei + '_dev'] = iface.name
 
